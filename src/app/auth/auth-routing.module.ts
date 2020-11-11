b1732e3a522296f5e37b581f6f4f033b4229249b
Angel Hurtado <contact@angelxehg.com>
2020-11-11 17:03:11 -0600

    Route inside CentralCardLayout

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NoAuthGuard } from '../guards/no-auth.guard';
+import { CentralCardLayoutComponent } from '../layouts/central-card-layout/central-card-layout.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/auth/login',
-    pathMatch: 'full'
+    component: CentralCardLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        canActivate: [NoAuthGuard],
+        component: LoginComponent
+      },
+      {
+        path: '',
+        redirectTo: '/auth/login',
+        pathMatch: 'full'
+      }
+    ]
   },
-  {
-    path: 'login',
-    canActivate: [NoAuthGuard],
-    component: LoginComponent
-  }
 ];
 
 @NgModule({
