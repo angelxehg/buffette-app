b701ddd877ba2228dcd4636d0122c5612caae8f8
Angel Hurtado <contact@angelxehg.com>
2020-11-11 15:32:56 -0600

    Use PanelLayout for most routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,43 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PanelLayoutComponent } from './layouts/panel-layout/panel-layout.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/auth/login',
-    pathMatch: 'full'
-  },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
   },
   {
-    path: 'app',
-    redirectTo: '/app/home',
-    pathMatch: 'full'
+    path: '',
+    component: PanelLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
+      },
+      {
+        path: 'products',
+        loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule),
+      },
+      {
+        path: 'sales',
+        loadChildren: () => import('./pages/sales/sales.module').then(m => m.SalesModule),
+      },
+      {
+        path: 'supplies',
+        loadChildren: () => import('./pages/supplies/supplies.module').then(m => m.SuppliesModule),
+      },
+      {
+        path: 'inventories',
+        loadChildren: () => import('./pages/inventories/inventories.module').then(m => m.InventoriesModule),
+      },
+      {
+        path: '',
+        redirectTo: '/auth/login',
+        pathMatch: 'full'
+      }
+    ]
   },
-  {
-    path: 'app/home',
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
-  },
-  {
-    path: 'app/products',
-    loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule),
-  },
-  {
-    path: 'app/sales',
-    loadChildren: () => import('./pages/sales/sales.module').then(m => m.SalesModule),
-  },
-  {
-    path: 'app/supplies',
-    loadChildren: () => import('./pages/supplies/supplies.module').then(m => m.SuppliesModule),
-  },
-  {
-    path: 'app/inventories',
-    loadChildren: () => import('./pages/inventories/inventories.module').then(m => m.InventoriesModule),
-  }
 ];
 
 @NgModule({
