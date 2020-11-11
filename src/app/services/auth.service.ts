79c2b5c2ea5b2bcc9f4d7650310ebee60f1fe200
Angel Hurtado <contact@angelxehg.com>
2020-11-11 16:57:32 -0600

    Read token from localStorage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,22 +14,22 @@ export class AuthService {
   public sessionStatus = new BehaviorSubject<boolean>(null);
 
   constructor(private router: Router) {
-    if (1 === 1) {
-      // Load session from storage
-      setTimeout((obs: BehaviorSubject<boolean>) => {
-        this.sessionStatus.next(false);
-      }, 1000, this.sessionStatus);
+    const token = localStorage.getItem('JWT_TOKEN');
+    if (token) {
+      this.sessionStatus.next(true);
     } else {
-      // Do not load from storage
+      this.sessionStatus.next(false);
     }
   }
 
   public login(): void {
+    localStorage.setItem('JWT_TOKEN', 'dummy_token');
     this.sessionStatus.next(true);
     this.router.navigateByUrl('/home');
   }
 
   public logout(): void {
+    localStorage.removeItem('JWT_TOKEN');
     this.sessionStatus.next(false);
     this.router.navigateByUrl('/auth/login');
   }
