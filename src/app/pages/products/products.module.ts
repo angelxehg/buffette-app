4783c67d461284cd177696a2bafd3e7310036210
Angel Hurtado <contact@angelxehg.com>
2020-11-11 19:08:24 -0600

    Create ProductDetails Component

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { ProductsRoutingModule } from './products-routing.module';
 import { LayoutsModule } from 'src/app/layouts/layouts.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,
